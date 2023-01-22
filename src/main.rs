c0502e6f549d846703371baed7520834f086ac3b
Levi Zhang <zhanghuayu.dev@gmail.com>
2023-01-22 11:23:01 +0000

    inint

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    // println!("Guess:");
+
+    // let mut guess = String::new();
+    let x = 5;
+    let y = 7;
+
+    // io::stdin()
+    //     .read_line(&mut guess)
+    //     .expect("Failed to read line...");
+
+    println!("x = {x}, y = {}", y*2);
+
+    // print!("You guessed: {guess}");
+}
