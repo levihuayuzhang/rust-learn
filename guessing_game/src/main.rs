1b4438b8b968bdcc9c664a16d419cbe490c831d1
Huayu Zhang <zhanghuayu.dev@gmail.com>
2025-05-13 15:53:36 +0800

    add rustlings
    Signed-off-by: Huayu Zhang <zhanghuayu.dev@gmail.com>

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,13 @@
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Guess the number! U fucking idot!");
-    println!("Please in put your fucking guess!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
+    println!("Just in put your fucking guess!");
 
     let mut guess = String::new();
 
@@ -10,6 +15,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read the fucking line!!!");
 
+    let guess: u32 = guess.trim().parse().expect("Input integer");
+
     println!("You guessed: {}", guess);
 
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small, dumb~"),
+        Ordering::Greater => println!("Too big, lmao~"),
+        Ordering::Equal => println!("It's right, u lucky buster!"),
+    }
 }
