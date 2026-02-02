6ea7a945b3387dbc3d232b5107c5accae2b7447f
Huayu Zhang <zhanghuayu.dev@gmail.com>
2026-02-02 09:34:44 +0800

    the book: finished.
    Signed-off-by: Huayu Zhang <zhanghuayu.dev@gmail.com>

--- the-book-practices/hello/src/main.rs
+++ the-book-practices/hello/src/main.rs
@@ -1,28 +1,48 @@
+// use libc::pthread_self;
+use hello::ThreadPool;
 use std::{
     fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(6);
 
-    for stream in listener.incoming() {
+    // for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+
+            // #[cfg(debug_assertions)]
+            // dbg!(thread::current().id());
+            // dbg!(unsafe { pthread_self() }); // macos
+            // dbg!(unsafe { syscall(SYS_gettid) }); // linux
+        });
+
+        println!("Server is shutting down!")
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            let t = 5;
+            thread::sleep(Duration::from_secs(t));
+            println!("Sleeped for {t} seconds...");
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
+
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
