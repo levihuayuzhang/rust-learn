1ddd15b1fd68344ab0662c296bbabcf992350c3e
Huayu Zhang <zhanghuayu.dev@gmail.com>
2026-02-07 09:51:06 +0800

    backup: 2026-2-7.
    Signed-off-by: Huayu Zhang <zhanghuayu.dev@gmail.com>

--- tutorials/async-tutorial/tokio-test/src/lib.rs
+++ tutorials/async-tutorial/tokio-test/src/lib.rs
@@ -8,7 +8,17 @@ mod tests {
 
     #[test]
     fn test_double() {
-        let result = double(2);
-        assert_eq!(result, 4);
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        assert_eq!(rt.block_on(double(2)), 4);
+    }
+
+    #[tokio::test]
+    // #[tokio::test(flavor = "current_thread")]
+    // #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+    async fn test_double_tokio() {
+        assert_eq!(double(2).await, 4);
     }
 }
